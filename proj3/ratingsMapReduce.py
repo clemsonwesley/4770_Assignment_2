48d1a5cafd194eb0011aa816aa532f52eb388c69
Wesley K Knight <wkknigh@joey3.computing.clemson.edu>
2019-04-09 18:16:56 -0400

    Genres list has been added

--- proj3/ratingsMapReduce.py
+++ proj3/ratingsMapReduce.py
@@ -1,1 +1,16 @@
 #First MapReduce program
+
+#*************************************************************#
+#
+# Program Description: Finds the mean, median, and standard dev
+# movie genre using MapReduce
+#
+#*************************************************************#
+
+#!/usr/bin/env python
+
+import sys
+
+genres = ["Action", "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"]
+
+for line in sys.stdin:
